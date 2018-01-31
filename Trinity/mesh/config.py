6a7ec2f6cc6ca95351910cb0c40f4ba03158d626
Tomas Sheers <reikudjinn@gmail.com>
2018-01-31 11:29:48 +0100

    Update config.py

--- Trinity/mesh/config.py
+++ Trinity/mesh/config.py
@@ -4,4 +4,13 @@
 
 class Config():
   def __init__(self):
+    
+    # Relay configurations
+    relay = dict()
+    relay['process_time'] = 10 # Time to process, in microseconds
+    relay['process_variance'] = 0.05 # value between 0-1 representing how much the process time can vary
+    
+    # Cortex configurations
+    cortex = dict()
+    
     pass
