de0b53da3bcf8c1251a3bc71ed7388b582a74a11
reritom <reikudjinn@gmail.com>
2018-06-04 19:06:56 +0200

    Django aspect removed

--- Trinity/docs/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.mainDocs, name='mainDocs'),
-    url(r'^examples/$', views.exampleDocs, name='exampleDocs')
-]
--- Trinity/mesh/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.home, name='home'),
-    url(r'go/', views.gogo, name='gogo'),
-    url(r'examples/', views.examples, name='examples'),
-]
